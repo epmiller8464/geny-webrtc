b87c762c4d9def45ee8a187c9d54c9c3706b04b2
Eric Miller <epmiller8464@gmail.com>
2015-07-29 00:57:21 -0500

    Init

---
+++ geny-webrtc/RTC-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  RTC-BridgingHeader-.h
+//  geny-webrtc
+//
+//  Created by ghostmac on 7/29/15.
+//  Copyright (c) 2015 ghostmac. All rights reserved.
+//
+
+#ifndef geny_webrtc_RTC_BridgingHeader__h
+#define geny_webrtc_RTC_BridgingHeader__h
+#endif
+
+#import "RTCPeerConnectionFactory.h"
+#import "RTCVideoTrack.h"
+#import "SRWebSocket.h"
---
+++ geny-webrtc/RTCAppClient.swift
@@ -0,0 +1,13 @@
+//
+//  RTCAppClient.swift
+//  geny-webrtc
+//
+//  Created by ghostmac on 7/29/15.
+//  Copyright (c) 2015 ghostmac. All rights reserved.
+//
+
+import Foundation;
+//import RTCPeerConnectionFactory;
+
+let cd = RTCPeerConnectionFactory();
+//SRWebSocket
