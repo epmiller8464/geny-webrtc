3976bd0f442aca579928a62e085314184896fa9f
Eric Miller <epmiller8464@gmail.com>
2015-07-29 00:28:09 -0500

    Initial Commit

---
+++ geny-webrtcTests/geny_webrtcTests.swift
@@ -0,0 +1,36 @@
+//
+//  geny_webrtcTests.swift
+//  geny-webrtcTests
+//
+//  Created by ghostmac on 7/29/15.
+//  Copyright (c) 2015 ghostmac. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class geny_webrtcTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
